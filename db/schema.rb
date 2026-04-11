05f43792a8d9d07125e8d6500701f3fd7e853ab8
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-04-11 14:41:44 +0300

    Generate link model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_11_103012) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_11_114117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "url"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_links_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "confirmation_sent_at"
@@ -30,4 +38,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_11_103012) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "links", "users"
 end
