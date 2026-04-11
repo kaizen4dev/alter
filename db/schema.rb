a7afce1cac76869522719610ca0a295616668ab5
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-04-11 14:42:09 +0300

    Generate tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_11_114117) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_11_114154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_11_114117) do
     t.string "url"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_links_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +48,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_11_114117) do
   end
 
   add_foreign_key "links", "users"
+  add_foreign_key "tags", "users"
 end
