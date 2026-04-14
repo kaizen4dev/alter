9c3614eab61e8a07f03ab0e61ab58fcec97aeec2
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-04-15 00:02:31 +0300

    Handle tags param in LinksController

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,14 @@
 class LinksController < ApplicationController
   def index
     @links = Link.includes(:tags)
+
+    unless params[:tags].nil?
+      requiredTags = params[:tags].split
+
+      @links = @links.select do |link|
+        presentTags = link.tags.pluck(:name)
+        requiredTags.all? {|t| presentTags.include?(t)}
+      end
+    end
   end
 end
