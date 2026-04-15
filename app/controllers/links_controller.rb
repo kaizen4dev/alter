b38ffc5c2d5a1e7851d722c13418409ac97e4060
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-04-15 16:27:11 +0300

    Add ability to add links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,8 +7,37 @@ class LinksController < ApplicationController
 
       @links = @links.select do |link|
         presentTags = link.tags.pluck(:name)
-        requiredTags.all? {|t| presentTags.include?(t)}
+        requiredTags.all? { |t| presentTags.include?(t) }
       end
     end
+  end
+
+  def new
+    @link = current_user.links.new
+  end
+
+  def create
+    @link = current_user.links.new url: (link_params[:url])
+    @tags = current_user.tags
+
+    link_params[:tags].split.each do |name|
+      tag = @tags.find_by name: name
+      tag ||= @tags.create name: name
+      @link.tags.append tag
+    end
+
+    if @link.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def link_params
+    p = params.expect link: [ :url, :tags ]
+    p[:url] = "https://" + p[:url] unless p[:url].start_with?("https://", "http://")
+    p
   end
 end
